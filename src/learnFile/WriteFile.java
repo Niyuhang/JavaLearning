2eafc1b72cba0453347e8042414529f8c381fc6d
Niyuhang <niyuhang@aidigger.com>
2020-04-28 16:47:44 +0800

    add

--- src/learnFile/WriteFile.java
+++ src/learnFile/WriteFile.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 public class WriteFile {
     public static void main(String[] args) throws IOException {
@@ -12,13 +13,19 @@ public class WriteFile {
 
         try(
                 // 获取输出到文件的流
-                FileOutputStream fos = new FileOutputStream(writeFile);
+                FileOutputStream fos = new FileOutputStream(writeFile, true);
                 // 创建一个能够写入字符串的 连接输出文件流
                 OutputStreamWriter osw = new OutputStreamWriter(fos);
-                //
+                // 也可以利用PrintWrite 可以更方便的写入 例如换行
+                PrintWriter pw = new PrintWriter(osw);
                 ){
                     osw.write("123");
                     osw.flush();
+
+                    pw.println("测试呀");
+                    pw.println("你好呀");
+
+
         }
 
 
